88324e0462f7084a3fcbbb39aa9f0d56d5d62b42
Anjana Wijayawardhana <50132667+AnjanaWijayawardhana@users.noreply.github.com>
2025-08-21 16:44:27 +1000

    Update Implement.R

--- Real applications/Full data/Implement.R
+++ Real applications/Full data/Implement.R
@@ -78,7 +78,7 @@ gama.dash.start<-log((gama.start/(2-gama.start))+0.000001) # Add 0.000001, to ov
 # -----------------------------------------------------------
 
 
-N=15
+N=15000 #The number of VB iterations.
 
 
 ## 4.1 SEM with Gaussian errors
@@ -546,4 +546,5 @@ DIC.SEM.Gau$DIC1
 DIC.SEM.Gau.t$DIC1
 DIC.SEM.Gau.YJ$DIC1
 DIC.YJ.SEM.t$DIC1
+
 
